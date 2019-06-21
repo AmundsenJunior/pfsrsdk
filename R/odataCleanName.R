b788aa0f6dd6aac6cff3c36098c3629fcf1da39e
Scott Russell <srussell@coreinformatics.com>
2019-06-21 12:44:27 -0400

    RSDK-155 updated odataCleanName function comment for refType

--- R/odataCleanName.R
+++ R/odataCleanName.R
@@ -2,15 +2,16 @@
 #'
 #' \code{odataCleanName} Clean a name for ODATA.
 #' @param name  string to clean
-#' @param refType Reference to the type of object being passed.
+#' @param refType Reference to the type of object being passed (default: odataObject).
 #' @export
-#' @section Handling \code{refType} parameter:
-#' As of now the \code{refType} parameter defaults to the Value of "odataObject"
-#' This will ensure that the leading underscode is placed in front of numbers
-#' at the beginning of the odata string to comply with Odata Standards. However,
-#' This function can be used for other objects as well to replace spaces and
-#' hyphens with underscores.
-#'
+#' @section Using \code{refType} parameter:
+#' \code{refType} accepts two values, the default "odataObject" and "tenant".
+#' "odataObject" ensures that a leading underscore is placed in front of numbers
+#' at the beginning of the string to comply with OData Standards. This function
+#' can be used for other objects as well to replace spaces and hyphens with 
+#' underscores. When \code{refType} is set to "tenant", underscores are
+#' substituted for spaces, but leaves hyphens intact.
+#' 
 #' @return Returns name in ODATA compliant form
 #' @examples
 #' \dontrun{
